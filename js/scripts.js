7810c90594e27ab794f3cd5f747a9feffebf13c9
drewparker <drewjparker91@gmail.com>
2020-08-28 10:05:31 -0700

    Create size function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,47 @@
 //BUSINESS LOGIC
 function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = toppings
+  this.toppings = []
 }
+
+Pizza.prototype.sizeCost = function(size) {
+  let this.size
+  if (this.size === "Small") {
+    this.sizeCost = 10;
+  } else if (this.size === "Medium") {
+    this.sizeCost = 15
+  } else {
+    this.sizeCost = 20
+  }
+  return this.totalCost += this.sizeCost
+}
+
+Pizza.prototype.totaltoppings
+
+
+function toppingsArray()
+
+Pizza.prototype.toppingsArray = function(toppings) {
+  let pizzaToppings=[]
+  $("input:checkbox[name=topping]:checked").each(function(i) {
+    pizzaToppings[i] = $(this).val();
+  })
+}
+
+Pizza.prototype.totalCost = function() {
+  let totalCost = 0;
+  this.totalCost += this.toppingCost += this.sizeCost;
+  this.totalCost = totalCost;
+}
+
+
+
+
+
+
+
+
+
 
 
 
