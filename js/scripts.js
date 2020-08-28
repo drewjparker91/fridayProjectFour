1999d55ac20a041f4a36f214f0f0b5ca74cc3544
drewparker <drewjparker91@gmail.com>
2020-08-28 16:34:39 -0700

    Take out console logs and commented out notes

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.getPrice = function() {
-  // console.log(this.size)
   let sizeCost = 0
   let toppingCost = this.toppings.length
   if (this.size === "Small") {
@@ -15,7 +14,6 @@ Pizza.prototype.getPrice = function() {
   } else {
     sizeCost = 20;
   }
-  // console.log(toppingCost)
   return toppingCost += sizeCost
 }
 
@@ -26,19 +24,12 @@ $(document).ready(function(){
   $("#pizzaForm").submit(function(event){
     event.preventDefault();
     let toppingsArray = [];
-    // console.log("dfddf")
     $("input:checkbox[name=toppings]:checked").each(function(i) {
       toppingsArray[i] = $(this).val(); //[i] because i want the value to be the length of the index count?
-      // console.log("hi")
     })
 
     let size = $("#size").val();
     console.log(size);
-
-    // let check = $("#size").val();
-    // console.log(check);
-
-    // console.log(toppingsArray);
 
 
     let pizza = new Pizza (size, toppingsArray);
@@ -56,43 +47,3 @@ $(document).ready(function(){
 //gather topping input in an array
 //gather price of topping input
 //Add sizeCost and toppingCost to get totalCost
-
-
-// totalCost === this.toppings += this.size
-
-
-// Pizza.prototype.toppingCost = 
-
-
-// function toppingsArray()
-
-
-// Pizza.prototype.toppingCost = function() {
-//   return toppingsArray.length; // cost of toppings is the length of the array because each topping is $1
-// } 
-
-// function toppings() {
-//   let toppingsArray = [];
-//   $("input:checkbox[name=toppings]:checked").each(function() {
-//     toppingsArray[i] = ($(this).val()); //[i] because i want the value to be the length of the index count?
-//   })
-//   return toppingsArray; 
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Pizza.prototype.totalCost = function() {
-//   let totalCost = 0;
-//   this.totalCost += this.toppingCost += this.sizeCost;
-//   this.totalCost = totalCost;
-// }
