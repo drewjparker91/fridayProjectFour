944a6c7686ed0419f3f6801ddc89c64036787e62
drewparker <drewjparker91@gmail.com>
2020-08-28 10:45:47 -0700

    research turning list of toppings into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 //BUSINESS LOGIC
 function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = []
+  this.toppings = [];
 }
 
 Pizza.prototype.sizeCost = function(size) {
-  let this.size
   if (this.size === "Small") {
     this.sizeCost = 10;
   } else if (this.size === "Medium") {
@@ -16,32 +15,53 @@ Pizza.prototype.sizeCost = function(size) {
   return this.totalCost += this.sizeCost
 }
 
-Pizza.prototype.totaltoppings
 
-
-function toppingsArray()
-
-Pizza.prototype.toppingsArray = function(toppings) {
-  let pizzaToppings=[]
-  $("input:checkbox[name=topping]:checked").each(function(i) {
+function toppings(){
+  let pizzaToppings = []
+  $("input:checkbox[name=toppings]:checked").each(function(i){
     pizzaToppings[i] = $(this).val();
   })
+  return pizzaToppings
 }
 
-Pizza.prototype.totalCost = function() {
-  let totalCost = 0;
-  this.totalCost += this.toppingCost += this.sizeCost;
-  this.totalCost = totalCost;
-}
+Pizza.prototype.totalToppings = function(){
+  this.toppings.forEach (function(){
+
+  })
+} 
 
 
 
 
+//USER LOGIC
+$(document).ready(function(){
+  $("#pizzaForm").submit(function(event){
+    event.preventDefault();
+    $("#")
+
+  })
+
+})
 
 
 
+totalCost === this.toppings += this.size
 
 
+// Pizza.prototype.toppingCost = 
 
 
+// function toppingsArray()
 
+// Pizza.prototype.toppingsArray = function(toppings) {
+//   let pizzaToppings=[]
+//   $("input:checkbox[name=topping]:checked").each(function(i) {
+//     pizzaToppings[i] = $(this).val();
+//   })
+// }
+
+// Pizza.prototype.totalCost = function() {
+//   let totalCost = 0;
+//   this.totalCost += this.toppingCost += this.sizeCost;
+//   this.totalCost = totalCost;
+// }
